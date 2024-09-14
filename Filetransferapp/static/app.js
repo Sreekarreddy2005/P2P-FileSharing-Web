75aaa31f57b068aa14897d6d3ced16f1335e48fb
Sreekarreddy2005 <167875374+Sreekarreddy2005@users.noreply.github.com>
2024-09-14 18:54:35 +0530

    Update app.js
    drag and drop

--- Filetransferapp/static/app.js
+++ Filetransferapp/static/app.js
@@ -1,4 +1,5 @@
 const socket = io();
+const dropZone = document.getElementById('drop-zone');
 const fileInput = document.getElementById('fileInput');
 const uploadButton = document.getElementById('uploadButton');
 const fileLink = document.getElementById('fileLink');
@@ -6,6 +7,26 @@ const shareOptions = document.getElementById('shareOptions');
 const copyLinkButton = document.getElementById('copyLinkButton');
 const emailLinkButton = document.getElementById('emailLinkButton');
 const whatsappLinkButton = document.getElementById('whatsappLinkButton');
+
+dropZone.addEventListener('click', () => {
+    fileInput.click();
+});
+
+dropZone.addEventListener('dragover', (e) => {
+    e.preventDefault();
+    dropZone.classList.add('hover');
+});
+
+dropZone.addEventListener('dragleave', () => {
+    dropZone.classList.remove('hover');
+});
+
+dropZone.addEventListener('drop', (e) => {
+    e.preventDefault();
+    dropZone.classList.remove('hover');
+    const files = e.dataTransfer.files;
+    fileInput.files = files;
+});
 
 uploadButton.addEventListener('click', () => {
     const file = fileInput.files[0];

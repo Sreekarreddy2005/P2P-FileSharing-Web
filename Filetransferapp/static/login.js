cc02f64f46e3fd75964eba522f40ae34f45f7918
Sreekarreddy2005 <167875374+Sreekarreddy2005@users.noreply.github.com>
2024-09-15 14:08:25 +0530

    Add files via upload

---
+++ Filetransferapp/static/login.js
@@ -0,0 +1,25 @@
+document.getElementById('loginSubmit').addEventListener('click', function(event) {
+  event.preventDefault();
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+
+  fetch('/login', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ email: email, password: password })
+  })
+  .then(response => response.json())
+  .then(data => {
+      if (data.error) {
+          document.getElementById('errorMessage').textContent = data.error;
+      } else {
+          document.cookie = "loggedIn=true; path=/";
+          window.location.href = '/';
+      }
+  })
+  .catch(error => {
+      console.error('Error:', error);
+  });
+});

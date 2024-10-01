3ad18d050883837368f15f4eb49da4c9ce2b568d
Sreekarreddy2005 <167875374+Sreekarreddy2005@users.noreply.github.com>
2024-10-01 16:50:38 +0530

    Update login.js
    till qrcode

--- Filetransferapp/static/login.js
+++ Filetransferapp/static/login.js
@@ -1,25 +1,31 @@
-document.getElementById('loginSubmit').addEventListener('click', function(event) {
-  event.preventDefault();
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
+document.addEventListener('DOMContentLoaded', function() {
+    const loginSubmit = document.getElementById('loginSubmit');
 
-  fetch('/login', {
-      method: 'POST',
-      headers: {
-          'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ email: email, password: password })
-  })
-  .then(response => response.json())
-  .then(data => {
-      if (data.error) {
-          document.getElementById('errorMessage').textContent = data.error;
-      } else {
-          document.cookie = "loggedIn=true; path=/";
-          window.location.href = '/';
-      }
-  })
-  .catch(error => {
-      console.error('Error:', error);
-  });
+    if (loginSubmit) {
+        loginSubmit.addEventListener('click', function(event) {
+            event.preventDefault();
+            const email = document.getElementById('email').value;
+            const password = document.getElementById('password').value;
+            
+            fetch('/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ email: email, password: password })
+            })
+            .then(response => response.json())
+            .then(data => {
+                if (data.error) {
+                    document.getElementById('errorMessage').textContent = data.error;
+                } else {
+                    document.cookie = "loggedIn=true; path=/";
+                    window.location.href = '/';
+                }
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+        });
+    }
 });
